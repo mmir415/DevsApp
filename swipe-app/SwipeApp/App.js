aefddbf2b7ac30d64315e3fb9f0bd21415a2e11c
mmir415 <41189871+mmir415@users.noreply.github.com>
2019-07-04 00:12:34 +1200

    Made a Login page for the proper app

--- swipe-app/SwipeApp/App.js
+++ swipe-app/SwipeApp/App.js
@@ -1,25 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import {TestComponent} from './testComponent';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app, dawg!</Text>
-      {/* Instantiating multiple instances of the component */}
-      <TestComponent name={'Chinmay'} age={20}/>
-      <TestComponent name={'Kungeng'} age={25}/>
-      <TestComponent name={'Terence'} age={12}/>
-      <TestComponent name={'Hassaan'} age={40}/>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex:1,
-    backgroundColor: 'pink',
-    justifyContent:'center',
-    alignItems:'center'
+import { LoginScreen } from "./screens/LoginScreen";
+export default class App extends React.Component {
+  render() {
+    return (
+      <LoginScreen />
+    );
   }
-})
+}
