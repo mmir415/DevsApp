aa3c47eab6a4ea61d7d4799174006e37df9a5aeb
mmir415 <41189871+mmir415@users.noreply.github.com>
2019-07-04 21:22:06 +1200

    Updated Material Button for Workshop 3

--- swipe-app/SwipeApp/components/MaterialButton/MaterialButton.js
+++ swipe-app/SwipeApp/components/MaterialButton/MaterialButton.js
@@ -1,4 +1,4 @@
-    
+
 import React from "react";
 import { TouchableOpacity, Text } from "react-native";
 import styles from "./MaterialButton.styles";
@@ -6,7 +6,7 @@ import styles from "./MaterialButton.styles";
 export class MaterialButton extends React.Component {
     render() {
         return (
-            <TouchableOpacity style={styles.buttonContainer}>
+            <TouchableOpacity style={[this.props.buttonStyles, styles.buttonContainer]}>
                 <Text style={styles.buttonText}>{this.props.buttonText}</Text>
             </TouchableOpacity>
         );
