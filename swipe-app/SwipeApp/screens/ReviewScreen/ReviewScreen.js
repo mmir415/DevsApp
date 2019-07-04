b00e210df9808babbcae111bddac92be1fa4c2e9
mmir415 <41189871+mmir415@users.noreply.github.com>
2019-07-04 21:08:19 +1200

    Added Review Screen

---
+++ swipe-app/SwipeApp/screens/ReviewScreen/ReviewScreen.js
@@ -0,0 +1,13 @@
+    
+import React from "react";
+import { View, Text } from "react-native";
+
+export class ReviewScreen extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>Hello from the review screen</Text>
+            </View>
+        )
+    }
+}
